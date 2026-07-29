2e8dd59dd865b788ec9b2f442b2e86681ac281a1
Denzel <luckydenzel1@gmail.com>
2026-07-29 18:32:29 +0200

    [Project] Further worked on the console utility project. Changed the study log to be a .md format instead. Updated the daily study log accordingly.

--- daily_study_log.md
+++ daily_study_log.md
@@ -1,7 +1,19 @@
 This is my daily study log that will contain all my notes and logs of the working day. This will have about 520 logs, since that equates to 2 years of working on weekdays!
 
+### FND-01
+
 27-07-26:
+
 I learned how to create an issue template, how to set up a nice looking readme, and how to work with markup in text files to make the layout nicer.
 
+### FND-02
+
 28-07-26:
+
 I learned how to retrieve an element in an enum from its set value (eg. ElementName = 1. Where 1 is the value). I also learned how to check each individual char of a string with the .Any extension function to then check if the string contains any numbers.
+
+
+29-07-26
+
+I learned that when returning a static list (in a static class), it returns a reference to that list instead of a copy. 
+
--- projects/ConsoleUtilityTool/Program.cs
+++ projects/ConsoleUtilityTool/Program.cs
@@ -8,9 +8,7 @@
             UserInformation userInformation = GetUserInformation();
 
             Console.WriteLine("Got user information. Information:");
-            Console.WriteLine($"Name: {userInformation.name}");
-            Console.WriteLine($"Email: {userInformation.email}");
-            Console.WriteLine($"Age: {userInformation.age}");
+            PrintUserInformation(userInformation);
 
             while (true) {
 
@@ -29,9 +27,11 @@
 
                     while (true) {
                         userInput = Console.ReadLine();
-                        userInput.ToLower();
+                        userInput = userInput.ToLower();
 
-                        if (int.TryParse(userInput, out int userRespondsIndex) && userRespondsIndex < 5 && userRespondsIndex > 0) {
+                        if (int.TryParse(userInput, out int userRespondsIndex) && userRespondsIndex > 0 
+                            && userRespondsIndex <= Enum.GetValues(typeof(UserChangeInformationType)).Length) {
+
                             UserChangeInformationType informationType = (UserChangeInformationType)userRespondsIndex;
                             GetUserChangeInformationInput(ref userInformation, informationType);
 
@@ -42,9 +42,7 @@
                     }
 
                     Console.WriteLine("Updated user information. Information:");
-                    Console.WriteLine($"Name: {userInformation.name}");
-                    Console.WriteLine($"Email: {userInformation.email}");
-                    Console.WriteLine($"Age: {userInformation.age}");
+                    PrintUserInformation(userInformation);
                 } else {
                     while (true) {
                         Console.WriteLine("This is not a valid answer, please try again: ");
@@ -52,8 +50,11 @@
                 }
             }
 
-            // Rest of program...
-            return;
+            Console.WriteLine("\n You can start creating TODOs and this program will store them all in a nice, organized file");
+
+            ProgramOptions userProgramOptionChoice = GetUserProgramOptionsChoice();
+
+            
         }
 
         public static UserInformation GetUserInformation() {
@@ -160,8 +161,220 @@
                     break;
             }
         }
+
+        public static ProgramOptions GetUserProgramOptionsChoice() {
+            Console.WriteLine("Please input the number for one the according actions: ");
+            PrintAllProgramOptions();
+
+            while (true) {
+                // Get the user inputted number
+                GetUserInput(out string? inputIndexString, "Number: ", "This isn't a valid input, please try again!");
+
+                if (!int.TryParse(inputIndexString, out int inputIndex)) {
+                    Console.WriteLine("This isn't a valid number, make sure it doesn't include any letters!");
+                } else {
+                    if (inputIndex <= 0 || inputIndex > Enum.GetValues(typeof(ProgramOptions)).Length) {
+                        Console.WriteLine("This isn't a valid number index. Please specify a correct index");
+                    } else {
+                        return (ProgramOptions)inputIndex;
+                    }
+                }
+            }
+        }
+
+        public static void HandleUserProgramOptionChoice(ProgramOptions programOption) {
+            List<Todo> todosList = TodoHandler.GetTodosList();
+            int todoInputIndex;
+
+            switch (programOption) {
+                case ProgramOptions.Create:
+                    Todo createdTodo = CreateNewTodoFromUserInput();
+                    break;
+                case ProgramOptions.Edit:
+
+                    Console.WriteLine("Please specify the number of the todo you would like to edit: ");
+                    PrintAllTodosFromList(todosList);
+
+                    while (true) {
+                        // Get the user inputted number
+                        GetUserInput(out string? inputIndexString, "Todo Number: ", "This isn't a valid input, please try again!");
+
+                        if (!int.TryParse(inputIndexString, out todoInputIndex)) {
+                            Console.WriteLine("This isn't a valid number, make sure it doesn't include any letters!");
+                        } else {
+                            if (todoInputIndex <= 0 || todoInputIndex > todosList.Count + 1) {
+                                Console.WriteLine("This isn't a valid number index. Please specify a correct index");
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    Todo todoToEdit = todosList[todoInputIndex]; // Get the todo from the list which the user wants to edit
+
+                    // Add logic that makes the user able to edit the todo properly
+                    break;
+                case ProgramOptions.Remove:
+                    Console.WriteLine("Please specify the number of the according todo of which you want to remove: ");
+                    PrintAllTodosFromList(todosList);
+
+                    while (true) {
+                        // Get the user inputted number
+                        GetUserInput(out string? inputIndexString, "Todo Number: ", "This isn't a valid input, please try again!");
+
+                        if (!int.TryParse(inputIndexString, out todoInputIndex)) {
+                            Console.WriteLine("This isn't a valid number, make sure it doesn't include any letters!");
+                        } else {
+                            if (todoInputIndex <= 0 || todoInputIndex > todosList.Count + 1) {
+                                Console.WriteLine("This isn't a valid number index. Please specify a correct index");
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Are you sure?");
+                    while (true) {
+                        GetUserInput(out string? userResponse, "Y/N");
+
+                        userResponse = userResponse.ToLower();
+                        if (userResponse == "y") {
+                            break;
+                        } else if (userResponse == "n") {
+                            Console.WriteLine("Canceled opperation.");
+                            return;
+                        }
+                    }
+
+                    TodoHandler.RemoveTodo(todoInputIndex);
+
+                    Console.WriteLine("Removed TODO succesfully!");
+                    break;
+                case ProgramOptions.ViewAll:
+                    break;
+                case ProgramOptions.Export:
+                    break;
+            }
+        }
+
+        public static void PrintAllTodosFromList(List<Todo> todosList) {
+            int todoIndex = 1;
+            for (int i = 0; i < todosList.Count; i++) {
+                Console.WriteLine($"{todoIndex}: {todosList[i].name}");
+            }
+        }
+
+        public static Todo CreateNewTodoFromUserInput() {
+            Console.WriteLine("To create a new todo, please fill in the following information: ");
+            Console.WriteLine("Name: ");
+
+            string todoName;
+            string todoDescription;
+            string todoExpiration;
+            Todo.Importancy todoImportancy;
+
+            // Get the todo name
+            GetUserInput(out todoName, "Name: ");
+
+            // Get the todo description
+            GetUserInput(out todoDescription, "Specify all the information for the todo: ");
+
+            // Get the todo expiration
+            GetUserInput(out todoExpiration, "Specify the expiration date of this todo: ");
+
+
+            Console.WriteLine(
+                "Specify the importancy of this todo by inputting the according number:" +
+                "\n 1. Low" +
+                "\n 2. Medium" +
+                "\n 3. High"
+            );
+
+            // Get the todo importancy
+            while (true) {
+                // Get the importancy index
+                GetUserInput(out string? todoImportancyIndexString, "Number: ", "This isn't a valid input, please try again!");
+
+                if (!int.TryParse(todoImportancyIndexString, out int todoImportancyIndex)) {
+                    Console.WriteLine("This isn't a valid number, make sure it doesn't include any letters!");
+                } else {
+                    if (todoImportancyIndex <= 0 || todoImportancyIndex > Enum.GetValues(typeof(Todo.Importancy)).Length) {
+                        Console.WriteLine("This isn't a valid number index. Please specify a correct index");
+                    } else {
+                        todoImportancy = (Todo.Importancy)todoImportancyIndex;
+                        break;
+                    }
+                }
+            }
+
+            return new Todo(todoName, todoDescription, todoExpiration, todoImportancy);
+        }
+
+        public static void PrintUserInformation(UserInformation userInformation) {
+            // Print all three in the same log to avoid race condition logs that split up this log in half
+            Console.WriteLine(
+                $"Name: {userInformation.name}" +
+                $"\nEmail: {userInformation.email}" +
+                $"\nAge: {userInformation.age}"
+            );
+        }
+
+        public static void PrintAllProgramOptions() {
+            Console.WriteLine(
+                "1. Create a new TODO" +
+                "\n 2. Edit an existing TODO" +
+                "\n 3. Remove a TODO" +
+                "\n 4. View all TODOs" +
+                "\n 5. Export TODOs to file"
+            );
+        }
     }
 
+
+    public static class TodoHandler {
+        private static List<Todo> todosList = new List<Todo>();
+
+        public static void AddTodo(Todo todo) {
+            todosList.Add(todo);
+        }
+
+        public static void EditExistingTodo(int todoIndex, Todo replacementTodo) {
+            todosList[todoIndex] = replacementTodo; // Overwrite the old todo with the newly, edited todo
+        }
+
+        public static void RemoveTodo(int todoIndex) {
+            todosList.RemoveAt(todoIndex);
+        }
+
+        public static List<Todo> GetTodosList() {
+            return new List<Todo>(todosList);
+        }
+
+        public static void ExportTodosToFile() {
+
+        }
+    }
+
+    public class Todo {
+        public string name;
+        public string description;
+        public string dueDate;
+
+        public Importancy importancy;
+
+        public Todo(string name, string description, string dueDate, Importancy importancy) {
+            this.name = name;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.importancy = importancy;
+        }
+
+        public enum Importancy {
+            Low,
+            Medium,
+            High
+        }
+    }
 
     public class UserInformation {
         public string name;
@@ -181,7 +394,19 @@
         age = 3,
         multiple = 4
     }
+
+    public enum ProgramOptions {
+        Create = 1,
+        Edit = 2,
+        Remove = 3,
+        ViewAll = 4,
+        Export = 5
+    }
 }
+
+// TODO:
+// Add logic that makes the user able to edit the todo properly | Line 220
+// Create a function for fetching an index from the user input since there's a lot of code that is being copied
 
 // STUFF THAT CONSFUSED ME:
 
@@ -190,3 +415,5 @@
 
 // I couldn't figure out how to convert an index to the value of the UserChangeInformationType to then pass into the GetUserChangeInformationInput() function.
 // Took me about 20 minutes to figure out. Got the answer from this article: https://stackoverflow.com/questions/23563960/how-to-get-enum-value-by-string-or-int
+
+// WORKED DURATION: 4 hours
